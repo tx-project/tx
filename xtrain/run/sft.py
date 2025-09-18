c7a5024f9ea91d9626340f409e3c5c3ec54e88cf
Philipp Moritz <pcmoritz@gmail.com>
2025-09-18 01:14:50 -0700

    use batching

--- xtrain/run/sft.py
+++ xtrain/run/sft.py
@@ -27,20 +27,21 @@ def save_checkpoint(state: nnx.State, filename: str | os.PathLike) -> None:
 
 
 def loss_fn(model, batch):
-    logits = model(batch['text'][None,:])["logits"]
+    logits = model(batch['text'])["logits"]
     print("BBB logits", logits)
     loss = optax.softmax_cross_entropy_with_integer_labels(
-        logits=logits, labels=batch['target'][None,:]
+        logits=logits, labels=batch['target']
     )
     return loss.mean(), logits
 
 
-# @nnx.jit
+@nnx.jit
 def train_step(model, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, batch):
     grad_fn = nnx.value_and_grad(loss_fn, has_aux=True)
     (loss, logits), grads = grad_fn(model, batch)
     # metrics.update(loss=loss, logits=logits, labels=batch['label'])
     optimizer.update(model, grads)
+    return loss
 
     
 @app.command()
@@ -60,15 +61,17 @@ def main(
         loss=nnx.metrics.Average("loss"),
     )
 
-    ds: IterableDataset = train_dataset.with_format("numpy") # ty: ignore
-    for step, data in enumerate(ds.iter(batch_size=1)):
-        tokens = jnp.asarray(tokenizer(data["Text"][0])["input_ids"])
+    ds: IterableDataset = train_dataset #.with_format("numpy") # ty: ignore
+    for step, data in enumerate(ds.iter(batch_size=32)):
+        tokens = tokenizer(data["Text"], return_tensors="pt", padding=True)
+        batch = jnp.asarray(tokens["input_ids"])
         model.train()
         input_batch = {
-            "text": tokens[:-1],
-            "target": tokens[1:],
+            "text": batch[:,:-1],
+            "target": batch[:, 1:],
         }
-        train_step(model, optimizer, metrics, input_batch)
+        loss = train_step(model, optimizer, metrics, input_batch)
+        print("loss", loss)
 
     for metric, value in metrics.compute().items():
         print(metric, value)
