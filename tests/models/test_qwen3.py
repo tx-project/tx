548357947ca82b50c3f9fe3003b30eec26ea9ebd
Philipp Moritz <pcmoritz@gmail.com>
2025-09-18 14:54:35 -0700

    add conversion utility between model and safetensor keys

--- tests/models/test_qwen3.py
+++ tests/models/test_qwen3.py
@@ -7,25 +7,18 @@ from flax import nnx
 import numpy as np
 import safetensors.numpy
 import torch
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, PretrainedConfig
 from xtrain.models import Qwen3ForCausalLM
+from xtrain.utils import get_safetensors_key_mapping
 
 
-def load_checkpoint(filename: str | os.PathLike, model: nnx.Module) -> None:
-
-    def get_key(path: tuple) -> str:
-        if path[-1] in {"embedding", "kernel"}:
-            path = (*path[:-1], "weight")
-        return ".".join(map(str, path))
-    
+def load_checkpoint(filename: str | os.PathLike, config: PretrainedConfig, model: nnx.Module) -> None:
+    key_mapping = get_safetensors_key_mapping(config, model)
     tensors = safetensors.numpy.load_file(filename)
     model_params = nnx.to_flat_state(nnx.state(model))
     updates = []
     for path, param in model_params:
-        key = get_key(path)
-        if path[-2] == "lm_head" and key not in tensors:
-            # This model uses tied embeddings
-            key = "model.embed_tokens.weight"
+        key = key_mapping[path]
         tensors[key] = tensors[key].T
         if path[-2] in {"q_proj", "k_proj", "v_proj", "o_proj"}:
             tensors[key] = tensors[key].reshape(param.shape)
@@ -49,7 +42,7 @@ def test_qwen3():
 
         config = AutoConfig.from_pretrained("Qwen/Qwen3-0.6B")
         model = Qwen3ForCausalLM(config, rngs=nnx.Rngs(0))
-        load_checkpoint(Path(tmp) / "model.safetensors", model)
+        load_checkpoint(Path(tmp) / "model.safetensors", config, model)
         
         outputs = model(batch.input_ids.numpy(), attention_mask=batch.attention_mask.numpy(), output_hidden_states=True, output_attentions=True)
         assert np.allclose(hf_outputs.hidden_states[0], outputs["hidden_states"][0], rtol=1e-6)
---
+++ xtrain/utils.py
@@ -0,0 +1,20 @@
+from flax import nnx
+from transformers import PretrainedConfig
+
+
+def get_safetensors_key_mapping(config: PretrainedConfig, model: nnx.Module) -> dict[tuple, str]:
+    "Get the mapping from model parameter paths to safetensors keys."
+
+    def get_key(path: tuple) -> str:
+        if path[-1] in {"embedding", "kernel"}:
+            path = (*path[:-1], "weight")
+        return ".".join(map(str, path))
+
+    result = {}
+    model_params = nnx.to_flat_state(nnx.state(model))
+    for path, _ in model_params:
+        key = get_key(path)
+        if path[-2] == "lm_head" and config.tie_word_embeddings:
+            key = "model.embed_tokens.weight"
+        result[path] = key
+    return result
