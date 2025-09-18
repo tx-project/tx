Fix tests with batching and proper attention mask

tests/models/test_qwen3.py
@@ -38,9 +38,10 @@ def test_qwen3():
     tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
     hf_model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B", attn_implementation="eager", use_safetensors=True)
 
-    input_ids = tokenizer("The capital of France is", return_tensors="pt").input_ids
+    inputs = ["The capital of France is", "The most popular programming language is"]
+    batch = tokenizer(inputs, return_tensors="pt", padding=True)
     with torch.no_grad():
-        hf_outputs = hf_model(input_ids, output_hidden_states=True, output_attentions=True, return_dict=True)
+        hf_outputs = hf_model(batch.input_ids, attention_mask=batch.attention_mask, output_hidden_states=True, output_attentions=True, return_dict=True)
 
     # Save the HF model checkpoint so we can load our model from it
     with tempfile.TemporaryDirectory() as tmp:
@@ -50,7 +51,7 @@ def test_qwen3():
         model = Qwen3ForCausalLM(config, rngs=nnx.Rngs(0))
         load_checkpoint(Path(tmp) / "model.safetensors", model)
         
-        outputs = model(input_ids.numpy(), output_hidden_states=True, output_attentions=True)
+        outputs = model(batch.input_ids.numpy(), attention_mask=batch.attention_mask.numpy(), output_hidden_states=True, output_attentions=True)
         assert np.allclose(hf_outputs.hidden_states[0], outputs["hidden_states"][0], rtol=1e-6)
         assert np.allclose(hf_outputs.attentions[0], outputs["attentions"][0], rtol=1e-4)
         assert np.allclose(hf_outputs.hidden_states[1], outputs["hidden_states"][1], rtol=1e-3, atol=1e-3)