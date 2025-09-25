619b01b9ac18de1b3c5b174a68642d6571bf9834
Task Solver <user@example.com>
2025-09-25 21:10:13 +0000

    add loader for chat data

--- tx/loaders/__init__.py
+++ tx/loaders/__init__.py
@@ -1,5 +1,15 @@
+from typing import Iterator
+
+import jax
+
+from tx.loaders.chat import chat
 from tx.loaders.text import text
 
+
+LoaderIterator = Iterator[tuple[dict[str, jax.Array], dict[str, str]]]
+
+
 __all__ = [
+    "chat",
     "text"
 ]
--- tx/loaders/chat.py
+++ tx/loaders/chat.py
@@ -4,13 +4,13 @@ from transformers import AutoTokenizer, IterableDataset, PretrainedConfig
 from tx.loaders import LoaderIterator
 
 
-def text(config: PretrainedConfig, dataset: IterableDataset, batch_size: int) -> LoaderIterator:
-    "Data loader for text data. It returns an iterator over (batch, metrics) elements."
+def chat(config: PretrainedConfig, dataset: IterableDataset, batch_size: int) -> LoaderIterator:
+    "Data loader that applies the chat template. It returns an iterator over (batch, metrics) elements."
 
     tokenizer = AutoTokenizer.from_pretrained(config.name_or_path)
     for data in dataset.iter(batch_size=batch_size):
         # We pad to multiples of 128 here so jax needs to compile less different shapes
-        batch = tokenizer(data["text"], return_tensors="np", padding=True, pad_to_multiple_of=128)
+        batch = tokenizer.apply_chat_template(data["messages"], tokenize=True, return_tensors="np", padding=True, pad_to_multiple_of=128)
         batch = {k: jnp.asarray(v) for k, v in batch.items()}
         yield {
             "text": batch["input_ids"][:,:-1],
