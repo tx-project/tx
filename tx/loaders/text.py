f3b03469735728ee5baea90c26a81da1ef579dbc
Philipp Moritz <pcmoritz@gmail.com>
2025-10-01 12:33:35 -0700

    Save model config and tokenizer after training (#26)
    Make sure saved models can directly be served by storing the config and tokenizers. Also add instructions on how to run it.

--- tx/loaders/text.py
+++ tx/loaders/text.py
@@ -1,14 +1,13 @@
 import jax.numpy as jnp
 from datasets import Dataset
-from transformers import AutoTokenizer, PretrainedConfig
+from transformers import PreTrainedTokenizer
 
 from tx.loaders.common import LoaderIterator
 
 
-def text(config: PretrainedConfig, dataset: Dataset, batch_size: int) -> LoaderIterator:
+def text(tokenizer: PreTrainedTokenizer, dataset: Dataset, batch_size: int) -> LoaderIterator:
     "Data loader for text data. It returns an iterator over (batch, metrics) elements."
 
-    tokenizer = AutoTokenizer.from_pretrained(config.name_or_path)
     for data in dataset.iter(batch_size=batch_size):
         # We pad to multiples of 128 here so jax needs to compile less different shapes
         batch = tokenizer(data["text"], return_tensors="np", padding=True, pad_to_multiple_of=128)
